1cb36a6bc19674100d36505f2b336ac1fbc81e88
Göran Krampe <goran.krampe@gmail.com>
2020-05-18 14:34:51 +0200

    Implemented calculateHash and signng

--- lib/model/sample.dart
+++ lib/model/sample.dart
@@ -1,32 +1,36 @@
+import 'dart:typed_data';
+
 import 'package:json_annotation/json_annotation.dart';
+import 'package:nanodart/nanodart.dart';
+import 'package:validicitylib/model/block.dart';
 import 'package:validicitylib/model/key.dart';
 import 'package:validicitylib/util.dart';
 
 part 'sample.g.dart';
 
 @JsonSerializable()
-class Sample {
+class Sample extends Block {
   int id;
 
   DateTime created;
   DateTime modified;
 
-  /// Cryptographic hash of this record
-  // @Column(unique: true, indexed: true)
-  String hash;
-
   /// Previous record hash in the same Project, or Project hash.
   // @Column(unique: true)
   String previous;
 
-  /// Cryptographic signature of this record
-  String signature;
-
   /// Creator of this record
   String publicKey;
 
-  /// The serial identifier for the Sample, the NFC tag id?
+  /// The serial identifier for the Sample, the NFC tag.
   String serial;
+
+  /// Cryptographic hash of this record.
+  // @Column(unique: true, indexed: true)
+  String hash;
+
+  /// Cryptographic signature of this record.
+  String signature;
 
   /// The current state of the Sample's lifecycle
   // @Column(defaultValue: "'registered'")
@@ -38,10 +42,44 @@ class Sample {
 
   Map<String, dynamic> toJson() => _$SampleToJson(this);
 
-  /// Seal this Sample by signing.
-  seal(Key key, Sample previous) {}
+  /// Seal this Sample by adding previous hash and signing it.
+  seal(Key key, Sample previousSample) {
+    previous = previousSample.hash;
+    sign(key);
+  }
 
-  sign(Key key) {}
+  /// Sign this Sample by recording the publicKey of the signing key and then adding the signature.
+  sign(Key key) {
+    publicKey = key.publicKey;
+    key.signBlock(this);
+  }
 
-  calculateHash() {}
+  Uint8List intToBytes(int n) {
+    var bytes = ByteData(8);
+    bytes.setInt64(0, n);
+    return bytes.buffer.asUint8List();
+  }
+
+  Uint8List datetimeToBytes(DateTime n) {
+    return intToBytes(n.millisecondsSinceEpoch);
+  }
+
+  /// Convert all parts into bytes and then make a Blake2b hash.
+  String calculateHash() {
+    Uint8List idBytes = intToBytes(id);
+    Uint8List createdBytes = datetimeToBytes(created);
+    Uint8List modifiedBytes = datetimeToBytes(modified);
+    Uint8List previousBytes = NanoHelpers.hexToBytes(previous.padLeft(64, "0"));
+    Uint8List publicKeyBytes = NanoHelpers.hexToBytes(publicKey);
+    Uint8List serialBytes = NanoHelpers.stringToBytesUtf8(serial);
+    hash = NanoHelpers.byteToHex(Blake2b.digest256([
+      idBytes,
+      createdBytes,
+      modifiedBytes,
+      publicKeyBytes,
+      previousBytes,
+      serialBytes
+    ])).toUpperCase();
+    return hash;
+  }
 }
